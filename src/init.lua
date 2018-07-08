68b1e15066e9aed0871b381b9f0081e4f7e2f6a2
AmaranthineCodices <git@amaranthinecodices.me>
2018-07-08 12:04:02 -0400

    expose Store.None as top-level Blob.None

--- src/init.lua
+++ src/init.lua
@@ -3,6 +3,7 @@ local RoactIntegration = require(script.RoactIntegration)
 
 return {
 	Store = Store,
+	None = Store.None,
 	StoreProvider = RoactIntegration.Provider,
 	withStore = RoactIntegration.Consumer,
 }
